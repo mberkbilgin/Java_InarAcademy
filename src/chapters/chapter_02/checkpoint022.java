444c52f2752acfa197e8d3a112c416a4800f5de4
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-21 14:07:32 +0300

    new checkpoint022 homeworks

---
+++ src/chapters/chapter_02/checkpoint022.java
@@ -0,0 +1,17 @@
+ import java.util.Scanner ;
+
+ public class Checkpoint022 {
+
+ public static void main(String[] args) {
+	 // Create a Scanner object
+	Scanner input = new Scanner(System.in) ;
+       
+	System.out.println("Enter a number:") ;
+	
+	double number = input.nextDouble() ;
+
+	System.out.println(number + " is the number you entered!!  ") ;
+
+ }
+ }
+	
