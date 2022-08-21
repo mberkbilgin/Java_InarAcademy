9328da998193979d77f008162396f8b4c08cf424
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-21 14:53:57 +0300

    new Checkpoint026 homeworks

--- src/chapters/chapter_02/Checkpoint025.java
+++ src/chapters/chapter_02/Checkpoint025.java
@@ -1,6 +1,7 @@
 public class Checkpoint025{
 
 public static void main(String[] args) {
+	//all variable must be declared.
 	int k = 8 ;
 	int i = k + 2 ;
   
---
+++ src/chapters/chapter_02/Checkpoint026.java
@@ -0,0 +1,12 @@
+public class Checkpoint026 {
+
+public static void main(String[] args) {
+	int i, j, k ;
+
+		i = j = k = 2 ;
+
+		System.out.println(i+ " " + j + " " + k );
+
+}
+}
+
