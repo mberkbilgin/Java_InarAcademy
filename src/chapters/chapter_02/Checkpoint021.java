150c6a83d6e3e52e9e12ab50124f41aa3ffba64a
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-21 13:40:13 +0300

    new checkpoint homeworks

---
+++ src/chapters/chapter_02/Checkpoint021.java
@@ -0,0 +1,12 @@
+
+public class Checkpoint021 {
+
+public static void main(String[] args) {
+	double i = 50.0 ;
+		double k = i + 50.0 ;
+		double j = k + 1 ;
+	 
+	System.out.println( "j is " + j + " k is " + k  ) ;
+
+}
+}
