00ed4ea372d364d2f23a9367cbf5d3ad2cfa4cff
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-22 16:53:53 +0300

    new Checkpoint212 homeworks

---
+++ src/chapters/chapter_02/Checkpoint212.java
@@ -0,0 +1,16 @@
+package chapters.chapter_02;
+
+public class Checkpoint212 {
+	public static void main(String[] args) {
+		//answer of "If today is Tuesday, what will be the day in 100 days?"
+		
+		int remainingday = 100 % 7 ;
+	
+		//Today is found by adding remaining day to Tuesday
+		System.out.println("Today is Tuesday + remainingday" );
+		System.out.println("Tuesday" + remainingday);
+		System.out.println("Today is Thursday");
+				
+	}
+
+}
