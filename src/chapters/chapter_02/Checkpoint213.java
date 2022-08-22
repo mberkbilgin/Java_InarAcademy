0d35fcdba3f19160de9a888a66f72eae0cbead89
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-22 17:07:21 +0300

    new Checkpoint213 homeworks

---
+++ src/chapters/chapter_02/Checkpoint213.java
@@ -0,0 +1,18 @@
+package chapters.chapter_02;
+
+public class Checkpoint213 {
+
+	public static void main(String[] args) {
+	
+		//First expression for 25/4
+		System.out.println(25/4);
+		
+		//Second expression by being floating point number
+		float a = 25f / 4 ;
+		
+		System.out.println(a);
+
+		
+	}
+
+}
