040e0cde8e3670432e411745f5117f7247269885
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-23 16:31:19 +0300

    new Checkpoint127 homeworks

---
+++ src/chapters/chapter_02/Checkpoint217.java
@@ -0,0 +1,21 @@
+package chapters.chapter_02;
+
+import java.util.Scanner;
+
+public class Checkpoint217 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in) ;
+		System.out.println("Firstly enter number for m "
+				+ "secondly enter a number for r");
+		
+		float m = input.nextFloat() ;
+		
+		float r = input.nextFloat() ;
+		 
+		float result = m * r * r  ;
+		
+		System.out.println("resut is " + result);
+	}
+}
