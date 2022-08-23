97d8df075c82330475e88ddbbd73f2f4b914bdac
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-23 16:00:35 +0300

    new Checkpoint216 homeworks

---
+++ src/chapters/chapter_02/Checkpoint216.java
@@ -0,0 +1,12 @@
+package chapters.chapter_02;
+
+public class Checkpoint216 {
+
+	public static void main(String[] args) {
+ 
+		double number = Math.pow(2 , 3.5) ;
+		 System.out.println("number is " + number );
+
+	}
+
+}
