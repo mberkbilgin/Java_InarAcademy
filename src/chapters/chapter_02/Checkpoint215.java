6bbe9ef5ef3c28c61ed0c2b7d29d9977fbc8d868
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-23 15:55:39 +0300

    new Checkpoint215 homeworks

---
+++ src/chapters/chapter_02/Checkpoint215.java
@@ -0,0 +1,13 @@
+package chapters.chapter_02;
+
+public class Checkpoint215 {
+
+	public static void main(String[] args) {
+		System.out.println("25 / 4 is " + 25 / 4); // shown as int
+		System.out.println("25 / 4.0 is " + 25 / 4.0); // shown as double
+		System.out.println("3 * 2 / 4 is " + 3 * 2 / 4); // shown as int
+		System.out.println("3.0 * 2 / 4 is " + 3.0 * 2 / 4); //shown as double
+
+	}
+
+}
