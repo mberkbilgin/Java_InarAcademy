af1ca51164819705830949096611d692b95f65fb
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-24 14:42:48 +0300

    new Checkpoint226 homework

---
+++ src/chapters/chapter_02/Checkpoint226.java
@@ -0,0 +1,19 @@
+package chapters.chapter_02;
+
+public class Checkpoint226 {
+
+	public static void main(String[] args) {
+
+		int a = 6;
+		int b = a++;
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+		a = 6;
+		b = ++a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
