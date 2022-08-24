02489b145a6f27eb839a2847d97b6e75a4fd62e0
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-24 14:49:26 +0300

    new Checkpoint229 homework

---
+++ src/chapters/chapter_02/Checkpoint229.java
@@ -0,0 +1,13 @@
+package chapters.chapter_02;
+
+public class Checkpoint229 {
+
+	public static void main(String[] args) {
+		float f = 12.5F ;
+		int i = (int)f ;
+		System.out.println("f is " + f);
+		System.out.println("i is " + i);
+	}
+	
+
+}
