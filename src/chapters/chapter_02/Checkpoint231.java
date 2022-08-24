82bc26082687cf856b9aedcd5a4f4bf432aa5731
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-24 14:55:28 +0300

    new Checkpoint231 homework

---
+++ src/chapters/chapter_02/Checkpoint231.java
@@ -0,0 +1,13 @@
+package chapters.chapter_02;
+
+public class Checkpoint231 {
+
+	public static void main(String[] args) {
+		double amount = 5 ;
+		System.out.println(amount / 2); //This result is shown by double.
+		
+		System.out.println(5 / 2); //This result is shown by integer.
+		
+	}
+
+}
