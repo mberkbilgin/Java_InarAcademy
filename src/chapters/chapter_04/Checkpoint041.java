7cc9deb026d81bacc10be294194e23822c1c62e0
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-29 17:17:57 +0300

    new Checkpoint homework

---
+++ src/chapters/chapter_04/Checkpoint041.java
@@ -0,0 +1,62 @@
+package chapters.chapter_04;
+
+public class Checkpoint041 {
+
+	public static void main(String[] args) {
+		double a = Math.sqrt(4);
+		System.out.println(a);
+
+		double b = Math.sin(2 * Math.PI);
+		System.out.println(b);
+
+		double c = Math.cos(2 * Math.PI);
+		System.out.println(c);
+
+		double d = Math.pow(2, 2);
+		System.out.println(d);
+
+		double e = Math.log(Math.E);
+		System.out.println(e);
+
+		double f = Math.exp(1);
+		System.out.println(f);
+
+		double g = Math.max(2, Math.min(3, 4));
+		System.out.println(g);
+
+		double h = Math.rint(-2.5);
+		System.out.println("h "+h);
+
+		double m = Math.rint(2.5);
+		System.out.println("m "+m);
+
+		double i = Math.ceil(-2.5);
+		System.out.println("i "+i);
+
+		double n = Math.ceil(2.5);
+		System.out.println("n "+n);
+
+		double j = Math.floor(-2.5);
+		System.out.println("j "+j);
+
+		double o = Math.floor(2.5);
+		System.out.println("o "+o);
+
+		double k = Math.round(-2.5f);
+		System.out.println("k "+k);
+
+		double p = Math.round(2.5f);
+		System.out.println("p "+p);
+
+		double l = Math.round(-2.5);
+		System.out.println("l "+l);
+
+		double q = Math.round(2.5);
+		System.out.println("q "+q);
+
+		double r = Math.round(Math.abs(-2.5));
+		System.out.println("r "+r);
+
+	}
+
+}
