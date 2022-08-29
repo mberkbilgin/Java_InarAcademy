7fffda928d79c4785b1b344f1c7e43c2b0bdb0cb
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-29 17:40:34 +0300

    new Checkpoint homework

---
+++ src/chapters/chapter_04/Checkpoint043.java
@@ -0,0 +1,11 @@
+package chapters.chapter_04;
+
+public class Checkpoint043 {
+
+	public static void main(String[] args) {
+		double x = Math.toRadians(47);
+		System.out.println("47° is " + x +" radians.");
+		
+	}
+
+}
---
+++ src/chapters/chapter_04/Checkpoint044.java
@@ -0,0 +1,9 @@
+package chapters.chapter_04;
+
+public class Checkpoint044 {
+	public static void main(String[] args) {
+
+		double x = Math.toDegrees(Math.PI / 7);
+		System.out.println("π/7 is " + x + "°");
+	}
+}
