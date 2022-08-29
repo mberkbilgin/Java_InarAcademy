cd86ad5adceff3d164ed8269c024c14b155b275d
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-29 14:06:37 +0300

    new Assignment homework

--- src/weeks/week_03/Assignment03_2.java
+++ src/weeks/week_03/Assignment03_2.java
@@ -5,38 +5,40 @@ import java.util.Scanner;
 public class Assignment03_2 {
 
 	public static void main(String[] args) {
+
 		Scanner input = new Scanner(System.in);
 
-		System.out.println("Enter ages of three people : ");
+		System.out.println(" Please enter  ages of three people ");
+
 		int person1 = input.nextInt();
 		int person2 = input.nextInt();
 		int person3 = input.nextInt();
 
-		int max=0;
-		int min=0;
+		int max;
 
+		int min;
 		if (person1 > person2 && person1 > person3) {
 			max = person1;
 			if (person2 > person3) {
-				min = person3;} 
-			else {
-				min = person2;}} 
-		
-		else if (person2 > person1 && person2 > person3) {
+				min = person3;
+			} else
+				min = person2;
+		} else if (person2 > person1 && person2 > person3) {
 			max = person2;
 			if (person1 > person3) {
 				min = person3;
 			} else
-				min = person1;} 
-		else if (person3 > person1 && person3 > person2) {
-			max = person3;
-			if (person1 > person2) {
-				min = person2;
-			} else
 				min = person1;
-
+		} else {
+			max = person3;
+			if (person2 > person1) {
+				min = person1;
+			} else
+				min = person2;
 		}
-		System.out.println("The oldest age " + max + " the youngest age " + min);
+
+		System.out.println("The oldest age  " + max + " The youngest age " + min);
 
 	}
+
 }
