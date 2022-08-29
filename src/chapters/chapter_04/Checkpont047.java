48ca6195090fda2f0631b80ab7bd06e0bef7e12e
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-29 17:54:41 +0300

    new Checkpoint homework

---
+++ src/chapters/chapter_04/Checkpont047.java
@@ -0,0 +1,19 @@
+package chapters.chapter_04;
+
+public class Checkpont047 {
+
+	public static void main(String[] args) {
+		double x = Math.log(Math.exp(5.5)) ;
+		System.out.println("x is " + x);
+		
+		double y = Math.exp(Math.log(5.5)) ;
+		System.out.println("y is " +y);
+		
+		double z = Math.asin(Math.sin(Math.PI / 6));
+		System.out.println("z is " + z);
+		
+		double d = Math.sin(Math.asin(Math.PI / 6)) ;
+		System.out.println("d is "+ d);
+	}
+
+}
