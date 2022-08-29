ac8d3cf5ec30b52552c9fdf6ab878089ca1186ad
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-29 17:47:18 +0300

    new Checkpoint homework

---
+++ src/chapters/chapter_04/Checkpoint045.java
@@ -0,0 +1,16 @@
+package chapters.chapter_04;
+
+public class Checkpoint045 {
+
+	public static void main(String[] args) {
+		int x = (int)(Math.random()*22)+34 ;
+		System.out.println(x);
+		
+		int y = (int)(Math.random()*1000) ;
+		System.out.println(y);
+		
+		double z = (int)(Math.random()*51) +5.5 ;
+		System.out.println(z);
+	}
+
+}
