aac6da294ab301c23a807134916cb5a3d1e81271
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-30 23:50:45 +0300

    modified file

--- src/chapters/chapter_04/Checkpoint414.java
+++ src/chapters/chapter_04/Checkpoint414.java
@@ -3,7 +3,7 @@ package chapters.chapter_04;
 public class Checkpoint414 {
 
 	public static void main(String[] args) {
-		int x =  (int)(Math.random()*26) + 97 ;
+		int x =  (int)(Math.random()*26) + 'a' ;
 		char ch =(char)(x) ;
 		System.out.println(ch);
 
