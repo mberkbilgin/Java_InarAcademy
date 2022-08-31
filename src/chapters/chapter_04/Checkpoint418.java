4eee2a7ebb88733fa3d5f7d307300c5e6b301579
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-08-31 13:19:09 +0300

    new Checkpoint homeworks

---
+++ src/chapters/chapter_04/Checkpoint418.java
@@ -0,0 +1,15 @@
+package chapters.chapter_04;
+
+public class Checkpoint418 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("1" + 1);
+		System.out.println('1'+ 1); //unicode of '1' is 49
+		System.out.println("1"+1+1);
+		System.out.println("1"+ (1+1));
+		System.out.println('1'+1+1);;
+		
+	}
+
+}
---
+++ src/chapters/chapter_04/Checkpoint419.java
@@ -0,0 +1,12 @@
+package chapters.chapter_04;
+
+public class Checkpoint419 {
+
+	public static void main(String[] args) {
+		System.out.println(1+ "Welcome"+1+1);
+		System.out.println(1+ "Welcome"+(1+1));
+		System.out.println(1+ "Welcome"+('\u0001'+1));
+		System.out.println(1+ "Welcome"+'a'+1);
+	}
+
+}
