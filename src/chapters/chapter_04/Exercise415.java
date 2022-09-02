d7d1f1b00cd77eeb5811121ce2549bda128a85a6
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-09-02 17:41:01 +0300

    new Exercise416

--- src/chapters/chapter_04/Exercise415.java
+++ src/chapters/chapter_04/Exercise415.java
@@ -9,35 +9,40 @@ public class Exercise415 {
 		System.out.println("Enter a letter:");
 		String letter = input.next();
 		letter = letter.toLowerCase();
+
 		if (letter.length() != 1) {
 			System.out.println("Please enter one character!");
 		} else {
+
 			char ch = letter.charAt(0);
+			if (Character.isLetter(ch)) {
 
-			if (ch == 'a' || ch == 'b' || ch == 'c') {
-				System.out.println("The corresponding number is 2");
-			} else if (ch == 'd' || ch == 'e' || ch == 'f') {
-				System.out.println("The corresponding number is 3");
+				if (ch == 'a' || ch == 'b' || ch == 'c') {
+					System.out.println("The corresponding number is 2");
+				} else if (ch == 'd' || ch == 'e' || ch == 'f') {
+					System.out.println("The corresponding number is 3");
 
-			} else if (ch == 'g' || ch == 'h' || ch == 'i') {
-				System.out.println("The corresponding number is 4");
+				} else if (ch == 'g' || ch == 'h' || ch == 'i') {
+					System.out.println("The corresponding number is 4");
 
-			} else if (ch == 'j' || ch == 'k' || ch == 'l') {
-				System.out.println("The corresponding number is 5");
+				} else if (ch == 'j' || ch == 'k' || ch == 'l') {
+					System.out.println("The corresponding number is 5");
 
-			} else if (ch == 'm' || ch == 'n' || ch == 'o') {
-				System.out.println("The corresponding number is 6");
+				} else if (ch == 'm' || ch == 'n' || ch == 'o') {
+					System.out.println("The corresponding number is 6");
 
-			} else if (ch == 'p' || ch == 'q' || ch == 'r' || ch == 's') {
-				System.out.println("The corresponding number is 7");
+				} else if (ch == 'p' || ch == 'q' || ch == 'r' || ch == 's') {
+					System.out.println("The corresponding number is 7");
 
-			} else if (ch == 't' || ch == 'u' || ch == 'v') {
-				System.out.println("The corresponding number is 8");
+				} else if (ch == 't' || ch == 'u' || ch == 'v') {
+					System.out.println("The corresponding number is 8");
 
-			} else if (ch == 'w' || ch == 'x' || ch == 'y' || ch == 'z') {
-				System.out.println("The corresponding number is 9");
+				} else if (ch == 'w' || ch == 'x' || ch == 'y' || ch == 'z') {
+					System.out.println("The corresponding number is 9");
 
-			}
+				}
+			} else
+				System.out.println(ch + " is an invalid input");
 		}
 	}
 }
---
+++ src/chapters/chapter_04/Exercise416.java
@@ -0,0 +1,11 @@
+package chapters.chapter_04;
+
+public class Exercise416 {
+
+	public static void main(String[] args) {
+		int x = (int)(Math.random()*26 +65);
+		char ch = (char)(x)  ;
+		System.out.println(ch);
+	}
+
+}
