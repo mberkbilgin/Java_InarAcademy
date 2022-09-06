b90bd325ce929330939d8b4504c4df915113274d
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-09-06 18:43:46 +0300

    Checkpoint059

---
+++ src/chapters/chapter_05/Checkpoint/Checkpoint059.java
@@ -0,0 +1,11 @@
+package chapters.chapter_05.Checkpoint;
+
+public class Checkpoint059 {
+
+	public static void main(String[] args) {
+		for (int i=1 ; i<101 ; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
