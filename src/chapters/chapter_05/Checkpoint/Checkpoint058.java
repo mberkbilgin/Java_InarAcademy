66f16116b3d17651c8c504685ff746244a6d583b
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-09-06 18:41:25 +0300

    Checkpoint058

---
+++ src/chapters/chapter_05/Checkpoint/Checkpoint058.java
@@ -0,0 +1,20 @@
+package chapters.chapter_05.Checkpoint;
+
+public class Checkpoint058 {
+
+	public static void main(String[] args) {
+		int sumA = 0;
+
+		for (int i = 0; i < 10; ++i) {
+			sumA += i;
+		}
+		System.out.println("sumA is "+sumA);
+		
+		int sumB = 0;
+		for (int i = 0; i < 10; i++) {
+			sumB += i;
+		}
+		System.out.println("sumB is "+sumB);
+	}
+
+}
