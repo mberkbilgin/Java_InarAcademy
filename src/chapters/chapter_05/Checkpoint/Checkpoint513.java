dd5e552fe987595c8dbc5aa643277e969a8e7a2e
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-09-06 19:26:36 +0300

    Checkpoint513

---
+++ src/chapters/chapter_05/Checkpoint/Checkpoint513.java
@@ -0,0 +1,15 @@
+package chapters.chapter_05.Checkpoint;
+
+public class Checkpoint513 {
+
+	public static void main(String[] args) {
+		long sum = 0 ;
+		int i = 0 ;
+		while(i < 1000) {
+			i++ ;
+			sum = sum + i ; 
+		}
+	System.out.println("sum is "+sum);
+	}
+
+}
