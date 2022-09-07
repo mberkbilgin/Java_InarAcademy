26e2a50fb13c5613851c8ed2c16ee3ca15c8cfe1
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-09-07 15:05:52 +0300

    Checkpoint521

---
+++ src/chapters/chapter_05/Checkpoint/Checkpoint521.java
@@ -0,0 +1,27 @@
+package chapters.chapter_05.Checkpoint;
+
+import java.util.Scanner;
+
+public class Checkpoint521
+{
+
+	public static void main(String[] args) {
+		
+		Scanner input =  new Scanner(System.in);
+		
+		System.out.println("Enter first integer: ");
+		int n1 = input.nextInt();
+		System.out.println("Enter second integer: ");
+		int n2 = input.nextInt();
+		
+		int gcd = 1 ;
+		int k = 2 ; 
+		while ( k <= n1/2 && k <= n2/2 ) {
+			if  (n1 % k == 0 && n2 % k == 0 ) {
+				gcd = k ;}
+			k++ ;
+		}
+		System.out.println("The greatest common divisor for "+n1 + " and "+n2+" is "+gcd);
+	}
+
+}
