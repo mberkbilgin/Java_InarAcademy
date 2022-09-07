386624fd7fdba1de46362349fd7dc87fbf420fae
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-09-07 14:01:42 +0300

    Checkpoint520c-d

---
+++ src/chapters/chapter_05/Checkpoint/Checkpoint520c.java
@@ -0,0 +1,19 @@
+package chapters.chapter_05.Checkpoint;
+
+public class Checkpoint520c {
+
+	public static void main(String[] args) {
+		int i = 5 ;
+		while (i >= 1) {
+			int num = 1 ;
+			for (int j = 1 ; j <= i ; j++) {
+				System.out.println(num+"xxx");
+				num *= 2 ;
+			}
+			System.out.println();
+			i--;
+		}
+		
+	}
+
+}
