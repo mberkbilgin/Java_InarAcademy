b9677c06ce73ccb850fd3b6347977128631fa750
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-09-08 23:58:00 +0300

    Checkpoint526-A

---
+++ src/chapters/chapter_05/Checkpoint/Checkpoint526a.java
@@ -0,0 +1,18 @@
+package chapters.chapter_05.Checkpoint;
+
+public class Checkpoint526a {
+
+	public static void main(String[] args) {
+		int number = 0 ;
+		int sum = 0 ;
+		
+		while (number < 20 && sum <100) {
+		number++ ;
+		sum += number ;
+		}
+		System.out.println("The number is "+number);
+		System.out.println("The sum is "+sum);
+
+	}
+
+}
