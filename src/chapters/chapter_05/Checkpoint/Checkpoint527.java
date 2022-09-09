5418ef17c1a56eeffbba11364f8e26103c0243d5
M.Berk Bilgin <m.berkbilgin@outlook.com>
2022-09-09 17:06:52 +0300

    Checkpoint27-A

---
+++ src/chapters/chapter_05/Checkpoint/Checkpoint527.java
@@ -0,0 +1,18 @@
+package chapters.chapter_05.Checkpoint;
+
+public class Checkpoint527 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i < 4; i++) {
+			for (int j = 1; j < 4; j++) {
+				if (i * j > 2)
+					break;
+				System.out.println("i * j ="+i * j);
+			}
+
+			System.out.println("i is "+i);
+		}
+
+	}
+
+}
